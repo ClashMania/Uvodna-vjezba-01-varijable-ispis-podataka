14dcf18e6160b30f132a473a4ff496534c897e65
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-09-20 08:43:11 +0200

    Add files via upload

---
+++ Re-PrvaWindowsAplikacija/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Re_PrvaWindowsAplikacija
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a=100, b=3, x=a/b, y=a%b;
+
+            Console.WriteLine(a + " dijeljeno s " + b + " je " + x + " s ostatkom " + y);
+
+
+           Console.ReadKey();
+        }
+    }
+}
